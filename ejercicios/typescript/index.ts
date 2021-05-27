6e87e6558c99d81305f8507e3ee3d3b259ad23bf
Brian <jacklevybrsg@gmail.com>
2021-05-27 01:12:58 -0500

    Funciones typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -41,3 +41,22 @@ comodin = {type: 'Wildcard'}
 
 // Object
 let someObject: object = {type: 'Wildcard'}
+
+//Funciones 
+function add(a:number, b:number) : number {
+    return a + b
+}
+const sumF = add(4, 6)
+
+function createAdder(a:number): (number) => number {
+    return function (b:number) {
+        return b + a
+    }
+}
+
+const addFour = createAdder(4)
+const fourPlus6 = addFour(6)
+
+function fullName(firstName:string, lastName?:string): string {
+    return `${firstName} ${lastName}`
+}
